eb334d0b4cc74e30d066f81960b2b10eafba7b5f
Tim Chard <timchard@hotmail.com>
2017-09-05 03:37:06 +1000

    removed tokenization in inner loop

--- koppel11.py
+++ koppel11.py
@@ -23,37 +23,38 @@ from math import sqrt
 import jsonhandler
 import random
 import argparse
+import numpy as np
 
 from collections import Counter
 
 #--- Methods:
-'''- create Vector:
-gets a string (e.g. Book), splits it into and returns a vector
+'''- create dict:
+gets a string (e.g. Book), splits it into and returns a dict
 with all possible n-grams/features'''
 
 
-def createVector(s): 
-    vector = Counter([word[i:i + n] for word in s.split() for i in range(max(1,len(word) - n + 1))]) 
-    return vector
+def create_dict(s): 
+    dict = Counter([word[i:i + n] for word in s.split() for i in range(max(1,len(word) - n + 1))]) 
+    return dict
 
-'''- add:
-adds n-grams to our featurelist-vector, if is not included yet
- (containing all possible n-grams/features)'''
+def tokenize(s):
+    return [word[i:i + n] for word in s.split() for i in range(max(1,len(word) - n + 1))]
 
 
-def add(vector, ngram):
-    if ngram in vector:
-        vector[ngram] += 1
-    else:
-        vector[ngram] = 1
+def create_vector(tokens, corpus):
+    vec = np.zeros((len(corpus,)))
+    for i in tokens:
+        vec[corpus[i]] += 1
+    return vec
+
 
 '''- selectFeatures:
 selects the x most frequent n-grams/features (x=featureLength)
 to avoid a (possibly) too big featurelist'''
 
 
-def selectFeatures(vector):
-    return sorted(vector, key=vector.get, reverse=True)[:min(len(vector), featureLength)]
+def selectFeatures(word_counts):
+    return sorted(word_counts, key=word_counts.get, reverse=True)[:min(len(word_counts), featureLength)]
 
 '''- createFeatureMap:
 creates Feature Map that only saves
@@ -63,11 +64,18 @@ Thus, the featurelist needs less memory and can work faster'''
 
 def createFeatureMap(s, features):
     fmap = {}
-    vec = createVector(s)
+    vec = create_dict(s)
     for ngram in features:
         if ngram in vec:
             fmap[ngram] = vec[ngram]
     return fmap
+
+
+def create_feature_map_from_tokens(tokens, features):
+    vec = Counter(tokens)
+    fmap = {ngram : vec[ngram] for ngram in features if ngram in vec}
+    return fmap
+
 
 
 def cosMetric(v1, v2):
@@ -77,7 +85,7 @@ def minmaxMetric(v1, v2):
     return 1 - minmax(v1, v2)
 
 '''- cosSim:
-calculates cosine similarity of two vectors v1 and v2.
+calculates cosine similarity of two dicts v1 and v2.
 -> cosine(X, Y) = (X * Y)/(|X|*|Y|)
 '''
 
@@ -98,7 +106,7 @@ def cosSim(v1, v2):
     return sp / (len1 * len2)
 
 '''- minmax:
-calculates minmax similarity of two vectors v1 and v2.
+calculates minmax similarity of two dicts v1 and v2.
 -> minmax(X, Y) = sum(min(Xi, Yi))/sum(max(Xi, Yi))
 
 This baseline method will be used for further evaluation.
@@ -110,7 +118,7 @@ def minmax(v1, v2):
     maxsum = 0
     for ngram in v1:
         if ngram in v2:
-            # ngram is in both vectors
+            # ngram is in both dicts
             minsum += min(v1[ngram], v2[ngram])
             maxsum += max(v1[ngram], v2[ngram])
         else:
@@ -125,25 +133,25 @@ def minmax(v1, v2):
     return float(minsum) / maxsum
 
 '''- training:
-Turns a given string into a n-gram vector
+Turns a given string into a n-gram dict
 and returns its feature list.
 '''
 
 
 def training(s):
     print("training...")
-    vec = createVector(s)
+    vec = create_dict(s)
     print("selecting features...")
     fl = selectFeatures(vec)
     print("done")
     return fl
 
 '''- testSim:
-args: two vectors, a featurelist
+args: two dicts, a featurelist
 and func(to decide whether to use cosine or minmax similarity).
 
 uses createFeatureMap and cosSim or minmax
-and returns the similarity value of the two vectors
+and returns the similarity value of the two dicts
 '''
 
 
@@ -154,21 +162,6 @@ def testSim(x, y, fl, func):
         return cosSim(fx, fy)
     else:
         return minmax(fx, fy)
-
-
-
-def testMetric(x, y, fl, func):
-    fx = createFeatureMap(x, fl)
-    fy = createFeatureMap(y, fl)
-    if func == 0:
-        return cosMetric(fx, fy)
-    else:
-        return minmaxMetric(fx, fy)
-
-
-
-
-
 
 
 '''- getRandomString:
@@ -182,8 +175,13 @@ def getRandomString(s, length):
     r = random.randint(0, len(words) - length)
     return "".join(words[r:r + length])
 
-#--- main:
 
+def get_random_tokens(tokens, length):
+    r = random.randint(0, len(tokens) - length)
+    return tokens[r:r + length]
+
+
+#--- main:
 
 def main(corpusdir, outputdir):
     
@@ -215,32 +213,57 @@ def main(corpusdir, outputdir):
         if cand not in deletes:
             newcands.append(cand)
     candidates = newcands
-    words = [len(texts[cand].split()) for cand in texts]
-    minwords = min(words)
-    print(minwords)
 
-    fl = training(corpus)
+
+    words_counts = [len(texts[cand].split()) for cand in texts]
+    minwords = min(words_counts)
+
+    tokens = {cand : tokenize(texts[cand]) for cand in texts}
+    token_counts = [len(tokens[cand]) for cand in tokens]
+    min_tokens = min(token_counts)
+
+    print(minwords, min_tokens)
+
+    base_features = training(corpus)
+    feature_corpus = {base_features[i] : i for i in range(len(base_features))}
     authors = []
     scores = []
 
     for file in unknowns:
         print("testing " + file)
-        utext = jsonhandler.getUnknownText(file)
-        ulen = len(utext.split())
-        if ulen < minlen:
+
+        unknown_text = jsonhandler.getUnknownText(file)
+        unknown_len = len(unknown_text.split())
+        textlen = min(unknown_len, minwords)
+
+        #unknown_string = "".join(unknown_text.split()[:textlen])
+
+        unknown_tokens = tokenize(unknown_text)
+        token_len = min(len(unknown_tokens), min_tokens)
+
+        unknown_tokens = unknown_tokens[:token_len]
+        
+        if unknown_len < minlen:
             authors.append("None")
             scores.append(0)
         else:
             wins = [0] * len(candidates)
-            textlen = min(ulen, minwords)
+
             print(textlen)
-            ustring = "".join(utext.split()[:textlen])
+            
+            
             for i in range(repetitions):
-                rfl = random.sample(fl, len(fl) // 2)
+                sample_features = random.sample(base_features, len(base_features) // 2)
+                unknown_features = create_feature_map_from_tokens(unknown_tokens, sample_features)
+                
                 distances = []
                 for cand in candidates:
-                    candstring = getRandomString(texts[cand], textlen)
-                    distances.append(testMetric(candstring, ustring, rfl, 1))
+                    cand_tokens = get_random_tokens(tokens[cand], textlen)
+                    cand_features = create_feature_map_from_tokens(cand_tokens, sample_features)
+
+                    dist = minmaxMetric(cand_features, unknown_features)
+                    distances.append(dist)
+
                 wins[distances.index(min(distances))] += 1
             score = max(wins) / float(repetitions)
             if score >= threshold:
